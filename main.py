89a5f531528ed6eb6b58f92f89fc11f0ee914c70
DummyEgg <jaraxxeu@gmail.com>
2022-09-13 11:20:59 +0300

    Added Cache

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ API_TOKEN = ''
 
 logging.basicConfig(level=logging.INFO)
 
-cache = {"coin" : []} 
+cache = {} 
 
 bot = Bot(token=API_TOKEN)
 
 dp = Dispatcher(bot)
 
-def checkCoin():
-    pass
+def checkCoin(coin):
+    if coin in cache:
+        return cache[coin]
+    else:
+        t = cg.get_price(ids=coin, vs_currencies='usd', include_24hr_change='true')
+        if t == {}:
+            return None
+        else:
+            addCoin(coin, t) 
+            return t
 
-def addCoin():
-    pass
+async def addCoin(coin, t):
+    cache[coin] = t
+    await(30)
+    cache.pop(coin, None)
 
 
 def parseAndRequest(s):
@@ -34,11 +44,8 @@ def parseAndRequest(s):
 
     msg = []
     for coin, cnt in reqv:
-        if checkCoin(coin):
-            tmp = cg.get_price(ids=coin, vs_currencies='usd')
-            if tmp != {}:
-                msg.append([coin, tmp[coin]['usd'] * cnt])
-                addCoin(coin, tmp[coin]['usd'])
+        if checkCoin(coin) is not None:
+            msg.append([coin, cache[coin]])
     return msg
 
     
